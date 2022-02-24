49ef1baa6b4934f431f1d61b2b8fcd5b578a2b98
Renier <rhvandermerwe@gmail.com>
2022-02-24 18:55:39 +0200

    changes to polygon Id and csv output

--- convert_geojson_to_csv_arches.py
+++ convert_geojson_to_csv_arches.py
@@ -7,7 +7,7 @@ import pandas as pd
 from shapely.geometry import shape
 
 # set parent folder
-parent_folder = '/home/geo/Desktop/codebase/Arches-ETL/data/countries'
+parent_folder = 'C:\\Users\\Renier\\Desktop\\Admin_model_corrected_geometries\\country'
 
 # set the output file
 outputfile = os.path.join(parent_folder, 'Arches_export.csv')
@@ -48,23 +48,24 @@ df_lookup['Name Value'] = df_lookup['Name Value'].apply(lambda x: x[0])
 # set index to polygon name
 df_lookup = df_lookup.set_index('Name Value')
 
-print(df_lookup.loc['Botswana']['MAEASaM ID'])
-
 
 # read the json file
 def read_json(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding="utf-8") as f:
         data = json.load(f)
     return data
 
 # find the polygon id 
 def find_polygon_id(json_data):
-    if "NAME_1" in json_data["features"][0]['properties']:
-        print('Sub-district: ', json_data["features"][0]['properties']["NAME_2"])
-        polygon_name = json_data["features"][0]['properties']["NAME_2"]
-    elif "ADM1_NAME" in json_data["features"][0]['properties']:
-        print('District: ' + json_data["features"][0]['properties']["ADM1_NAME"])
-        polygon_name = json_data["features"][0]['properties']["ADM1_NAME"]
+    if "ADM_0" in json_data["features"][0]['properties']:
+        print('level0: ', json_data["features"][0]['properties']["ADM_0"])
+        polygon_name = json_data["features"][0]['properties']["ADM_0"]
+    elif "ADM_1" in json_data["features"][0]['properties']:
+        print('level1: ' + json_data["features"][0]['properties']["ADM_1"])
+        polygon_name = json_data["features"][0]['properties']["ADM_1"]
+    elif "ADM_2" in json_data["features"][0]['properties']:
+        print('level2: ' + json_data["features"][0]['properties']["ADM_2"])
+        polygon_name = json_data["features"][0]['properties']["ADM_2"]
     else:
         print("we don't know")
         print(json_data["features"][0]['properties'])
@@ -86,21 +87,21 @@ with open(outputfile, 'w') as f:
     f.write('"MAEASaM ID";"name";"geometry"\n')
     # loop through the list of countries
     for country in list_of_countries:
-        country_name = country.split('/')[-1]
+        country_name = country.split('\\')[-1]
         print(f'processing {country_name}')
         subfolders = list_folders(country)
         for subfolder in subfolders:
-            folder_name = subfolder.split('/')[-1]
+            folder_name = subfolder.split('\\')[-1]
             print(f'processing {folder_name}')
-            if folder_name == 'Districts':
-                districts = list_files(subfolder, extension = '_subdivided.json.geojson', include = True)
-                for district in districts:
-                    district_name = district.split('/')[-1]
-                    print('processing: ' + country_name + ' - ' + district_name)
+            
+            list_admin_div = list_files(subfolder, extension = '_subdivided.json.geojson', include = True)
+            for admin_div in list_admin_div:
+                    admin_div_name = admin_div.split('\\')[-1]
+                    print('processing: ' + country_name + ' - ' + admin_div)
                     
                     # read the json file
-                    json_data = read_json(district)
-                    print(district)
+                    json_data = read_json(admin_div)
+                    print(admin_div)
 
                     # find the polygon id
                     polygon_name, polygon_id = find_polygon_id(json_data)
@@ -111,26 +112,4 @@ with open(outputfile, 'w') as f:
                     # write the polygon id and wkt to a csv file
                     f.write(f'"{polygon_id}";"{polygon_name}";"{shape(g)}"\n')
 
-            if folder_name == 'sub-districts':
-                sub_districts_folders = list_folders(subfolder)
-                for sub_districts_folder in sub_districts_folders:
-                    district_name = sub_districts_folder.split('/')[-1]
-                    print('processing: sub districts of' + country_name + ' - ' + district_name)
-                    sub_districts_files = list_files(sub_districts_folder, extension = '_subdivided.json.geojson', include = True)
-                    for sub_districts_file in sub_districts_files:
-                        sub_districts_name = sub_districts_file.split('/')[-1]
-                        print('processing: ' + country_name + ' - ' + district_name + ' - ' + sub_districts_name)
-                        
-                        # read the json file
-                        json_data = read_json(sub_districts_file)
-                        print(sub_districts_file)
-                        # find the polygon id
-                        polygon_name, polygon_id = find_polygon_id(json_data)
-                        
-                        # get wkt of the geometry
-                        g = geojson.loads(json.dumps(json_data["features"][0]['geometry']))
-                        
-                        # write the polygon id and wkt to a csv file
-                        f.write(f'"{polygon_id}";"{polygon_name}";"{shape(g)}"\n')
-
-
+  
