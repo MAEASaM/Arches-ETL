97043c91fe67ff17bc84391c1df20b8c235dcc95
Renier <rhvandermerwe@gmail.com>
2022-02-24 09:12:27 +0200

    Parent folder changed. Stage one changed

--- country_divide.py
+++ country_divide.py
@@ -5,7 +5,7 @@ from qgis.core import QgsVectorLayer
 from pathlib import Path
 
 # set parent folder
-str_path = "C:\\Users\\razek\\Downloads\\arches_admin_convert"
+str_path = ""
 parent_folder = Path(str_path)
 print(parent_folder)
 
@@ -45,26 +45,38 @@ for country in list_of_countries:
     print(f'processing {country_name}')
     subfolders = list_folders(country)
     for subfolder in subfolders:
+        level0_folders = list_folders(subfolder)
         folder_name = subfolder.split('\\')[-1]
         print(f'processing folder: {folder_name}')
-        if folder_name == 'Districts':
-            districts = list_files(subfolder, extension = '_subdivided.json.geojson', include = False) 
-            for district in districts:
-                district_name = district.split('\\')[-1]
-                print('processing: ' + country_name + ' - ' + district_name)
-                district_layer = QgsVectorLayer(district,"distr","ogr")
-                processing.run("native:subdivide", {'INPUT': district_layer, "MAX_NODES":MAX_NODES, "OUTPUT":district[:-4]+'_subdivided.json'})
-        if folder_name == 'sub_districts':
-            sub_districts_folders = list_folders(subfolder)
-            for sub_districts_folder in sub_districts_folders:
-                district_name = sub_districts_folder.split('\\')[-1]
-                print('processing: sub districts of' + country_name + ' - ' + district_name)
-                sub_districts_files = list_files(sub_districts_folder, extension = '_subdivided.json.geojson', include = False)
-                for sub_districts_file in sub_districts_files:
-                    sub_districts_name = sub_districts_file.split('\\')[-1]
-                    print('processing: ' + country_name + ' - ' + district_name + ' - ' + sub_districts_name)
-                    sub_districts_layer = QgsVectorLayer(sub_districts_file,"sub_distr","ogr")
-                    processing.run("native:subdivide", {'INPUT': sub_districts_layer, "MAX_NODES":MAX_NODES, "OUTPUT":sub_districts_file[:-4]+'_subdivided.json'})
+        if folder_name == 'level0':
+            level0 = list_files(subfolder, extension = '_subdivided.json.geojson', include = False) 
+            for level0 in level0:
+                level0_name = level0.split('\\')[-1]
+                print('processing: ' + country_name + ' - ' + level0_name)
+                levl0 = QgsVectorLayer(level0,"amd0","ogr")
+                processing.run("native:subdivide", {'INPUT': level0, "MAX_NODES":MAX_NODES, "OUTPUT":level0[:-4]+'_subdivided.json'})
+    for subfolder in subfolders:
+        level1_folders = list_folders(subfolder)
+        folder_name = subfolder.split('\\')[-1]
+        print(f'processing folder: {folder_name}')
+        if folder_name == 'level1':
+            level1 = list_files(subfolder, extension = '_subdivided.json.geojson', include = False) 
+            for level1 in level1:
+                level1_name = level1.split('\\')[-1]
+                print('processing: ' + country_name + ' - ' + level0_name + '-' + level1_name)
+                levl1 = QgsVectorLayer(level1,"amd0","ogr")
+                processing.run("native:subdivide", {'INPUT': level1, "MAX_NODES":MAX_NODES, "OUTPUT":level1[:-4]+'_subdivided.json'})
+    for subfolder in subfolders:
+        level2_folders = list_folders(subfolder)
+        folder_name = subfolder.split('\\')[-1]
+        print(f'processing folder: {folder_name}')
+        if folder_name == 'level2':
+            level2 = list_files(subfolder, extension = '_subdivided.json.geojson', include = False) 
+            for level2 in level2:
+                level2_name = level2.split('\\')[-1]
+                print('processing: ' + country_name + ' - ' + level0_name + '-' + level1_name + '-' + level2_name)
+                levl2 = QgsVectorLayer(level2,"amd0","ogr")
+                processing.run("native:subdivide", {'INPUT': level2, "MAX_NODES":MAX_NODES, "OUTPUT":level2[:-4]+'_subdivided.json'})          
 
 
 '''
